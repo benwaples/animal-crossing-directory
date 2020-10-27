7fb63e647afa8343c4d4eed02f526a0d328a3695
bwapes <benwaples@gmail.com>
2020-10-26 20:13:53 -0700

    villager test

--- src/Components/presentation/Header/Header.tsx
+++ src/Components/presentation/Header/Header.tsx
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <div>
       <Link to="/villagers">See All Villagers</Link>
+      <h1>Animal Crossing Directory</h1> 
     </div>
   )
 }
---
+++ src/Components/presentation/Villager/Villager.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Villager } from './Villager'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('Villager component', () => {
+  it('should render a villager component', () => {
+    const { asFragment } = render(
+    <MemoryRouter>
+      <Villager 
+      name="agent 2"
+      imageUrl="show me the villager"
+      _id="villagerId"
+      />
+    </MemoryRouter>)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
