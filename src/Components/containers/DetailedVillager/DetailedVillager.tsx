753095354c7a6c92faa11dcdaefa05d6f14286fa
bwapes <benwaples@gmail.com>
2020-10-26 18:20:43 -0700

    dispaly details

--- src/Components/containers/DetailedVillager/DetailedVillager.tsx
+++ src/Components/containers/DetailedVillager/DetailedVillager.tsx
@@ -2,21 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { getVillager } from '../../../services/villager-api'
 
 type Props = {
-  match: { params: { _id: string}}
+  match: { params: { _id: string}},
+  name: string,
+
 }
 
+
 export const DetailedVillager: React.FC<Props> = ({match}) => {
-  const [villager, setVillager] = useState({})
+
+  const [villager, setVillager] = useState({name: '',image: '',personality: '',species: '',birthday: '',skill: '',quote: ''})
+
+  const [loading, setLoading ] = useState(true)
+  
   const id: string = match.params._id
 
   useEffect(() => {
     getVillager(id.substr(1))
       .then(villager => setVillager(villager))
-  }, [])
+      .finally(() => setLoading(false))
+  }, [id])
 
+  if(loading) return <h1>Loading...</h1>
   return (
     <div>
-      
+      <h1>{villager.name}</h1>
+      <img src={villager.image} alt={villager.name}/>
+      <p><b>Personality:</b> {villager.personality}</p>
+      <p><b>Species:</b> {villager.species}</p>
+      <p><b>Birthday:</b> {villager.birthday}</p>
+      <p><b>Skill:</b> {villager.skill}</p>
+      <p><b>Quote:</b> {villager.quote}</p>
     </div>
   )
 }
