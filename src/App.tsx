8d32dde2ac1546671987a4fe4acc23b5f6adf97c
bwapes <benwaples@gmail.com>
2020-10-26 18:25:00 -0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom'
 import { VillagerDirectory } from './Components/containers/VillagerDirectory/VillagerDirectory';
 import { DetailedVillager } from './Components/containers/DetailedVillager/DetailedVillager'
+import Header from './Components/presentation/Header/Header';
 
 
 function App() {
   return (
     <>
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/villagers" component={VillagerDirectory}/>
         <Route exact path="/villagers/:_id" component={DetailedVillager} />
